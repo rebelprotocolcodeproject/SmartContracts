ebd726d3a86afa15b6800f55b3a5cc8ae81a31e8
sanjay <sanjay529563@gmail.com>
2024-08-05 12:55:58 +0530

    pricefeed address

--- contracts/TokenPresale.sol
+++ contracts/TokenPresale.sol
@@ -226,7 +226,7 @@ contract Presale is Ownable, ReentrancyGuard {
         USDT = IERC20(_USDT);
         USDC = IERC20(_USDC);
         priceFeed = AggregatorV3Interface(
-            0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526
+            0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
         );
         tokenDecimals = mainToken.decimals();
         tokensToSell = 150000000 * 10 ** tokenDecimals;
